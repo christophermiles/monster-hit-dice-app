0243e0028d1b6e6e3c1dafcde1e887319023057e
Christoper Miles <mail@christophermiles.com.au>
2024-10-12 11:07:21 +1100

    Added table as reusable component

---
+++ components/HitPointResultsTable.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import type { HitPointResults } from 'roll-hit-dice/dist/types'
+import './HitPointResultsTable.css'
+
+interface HitPointResultsTableProps {
+  hitDice?: string
+  hitPointResults?: HitPointResults
+}
+
+const HitPointResultsTable: React.FC<HitPointResultsTableProps> = ({
+  hitDice,
+  hitPointResults,
+}) => {
+  return (
+    <table className="hit-point-results-table">
+      <thead>
+        <tr>
+          <th colSpan={5}>Hit Dice {hitDice ?? '_____'}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{hitPointResults?.minimum}</td>
+          <td>{hitPointResults?.weak}</td>
+          <td>{hitPointResults?.average}</td>
+          <td>{hitPointResults?.strong}</td>
+          <td>{hitPointResults?.maximum}</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th>Min.</th>
+          <th></th>
+          <th>Avg.</th>
+          <th></th>
+          <th>Max.</th>
+        </tr>
+      </tfoot>
+    </table>
+  )
+}
+
+export default HitPointResultsTable
