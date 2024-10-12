fa8d7d290fa64d5b7667302daa8cb1bd7c714d41
Christoper Miles <mail@christophermiles.com.au>
2024-10-13 00:49:17 +1100

    Added controls to results tables

--- components/HitPointResultsTable.tsx
+++ components/HitPointResultsTable.tsx
@@ -15,49 +15,60 @@ const HitPointResultsTable: React.FC<HitPointResultsTableProps> = ({
   monsterName,
   hitDice,
   hitPointResults,
-  dieType,
 }) => {
   return (
-    <table className="hit-point-results-table">
+    <div className="group">
       {hitPointResults && hitDice && (
-        <thead>
-          <tr>
-            <th colSpan={5}>
-              <span className={clsx('flex flex-col items-center')}>
-                {dieType && (
-                  <DiceIcon dieType={dieType} className="w-20 h-20" />
-                )}
+        <div
+          className={clsx(
+            'hit-point-results-table-header',
+            'grid grid-cols-3 grid-rows-1 items-center gap-8',
+          )}
+        >
+          <span />
 
-                <span>
-                  <strong className="font-medium">
-                    {monsterName ? monsterName : 'Hit Dice'} &mdash;{' '}
-                  </strong>
-                  {hitDice}
-                </span>
-              </span>
-            </th>
-          </tr>
-        </thead>
+          <h2 className="hit-point-results-table-heading">
+            <strong className="font-medium">
+              {monsterName ? monsterName : 'Hit Dice'} &mdash;{' '}
+            </strong>
+            {hitDice}
+          </h2>
+
+          <span
+            className={clsx(
+              'flex justify-end gap-2',
+              'text-sm',
+              'opacity-0 group-hover:opacity-100 transition-opacity',
+            )}
+          >
+            <button>Delete</button>
+
+            <button>Copy</button>
+          </span>
+        </div>
       )}
-      <tbody>
-        <tr>
-          <td>{hitPointResults?.minimum}</td>
-          <td>{hitPointResults?.weak}</td>
-          <td>{hitPointResults?.average}</td>
-          <td>{hitPointResults?.strong}</td>
-          <td>{hitPointResults?.maximum}</td>
-        </tr>
-      </tbody>
-      <tfoot>
-        <tr>
-          <th>Min.</th>
-          <th></th>
-          <th>Avg.</th>
-          <th></th>
-          <th>Max.</th>
-        </tr>
-      </tfoot>
-    </table>
+
+      <table className="hit-point-results-table">
+        <tbody>
+          <tr>
+            <td>{hitPointResults?.minimum}</td>
+            <td>{hitPointResults?.weak}</td>
+            <td>{hitPointResults?.average}</td>
+            <td>{hitPointResults?.strong}</td>
+            <td>{hitPointResults?.maximum}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <th>Minimum</th>
+            <th>Weak</th>
+            <th>Average</th>
+            <th>Strong</th>
+            <th>Maximum</th>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
   )
 }
 
