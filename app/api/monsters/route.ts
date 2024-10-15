af5090e965126e49cae2487ba7e967b00a5a1ab4
Christopher Miles <cmiles@unimelb.edu.au>
2024-10-15 14:04:10 +1100

    Reduced threshold for monster filtering

--- app/api/monsters/route.ts
+++ app/api/monsters/route.ts
@@ -61,6 +61,7 @@ export async function GET(request: NextRequest) {
 
     const fuse = new Fuse<Monster>(results, {
       keys: ['name'],
+      threshold: 0.25
     })
 
     return NextResponse.json(
