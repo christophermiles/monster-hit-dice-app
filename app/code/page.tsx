8ff61f7a1dc7fe9f43b088e050a862ef2bd65cad
Christopher Miles <mail@christophermiles.com.au>
2024-10-17 06:30:05 +1100

    Updated links to API and added example usage

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -17,9 +17,29 @@ export default function AboutApi() {
             target="_blank"
             className="link"
           >
-            api.hitdice.app
+            api.monsterhitdice.app
           </a>
           .
+        </p>
+
+        <p>For example:</p>
+
+        <code>
+          curl --request GET \<br />
+          --url
+          &#39;https://api.monsterhitdice.app/hp?hd=2d8-2&hd=2d8+6&hd=8d10+40&hd=33d20+330&#39;
+        </code>
+
+        <p>
+          Or try it from your browser:{' '}
+          <a
+            href="https://api.monsterhitdice.app/hp?hd=2d8-2&hd=2d8+6&hd=8d10+40&hd=33d20+330"
+            rel="nofollow"
+            target="_blank"
+            className="link"
+          >
+            api.monsterhitdice.app/hp?hd=2d8-2&hd=2d8+6&hd=8d10+40&hd=33d20+330
+          </a>
         </p>
 
         <p>
