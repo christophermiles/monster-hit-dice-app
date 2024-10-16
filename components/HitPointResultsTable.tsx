4130e2022c46fe4a72d075b708b0c13f3f666a2c
Christopher Miles <mail@christophermiles.com.au>
2024-10-17 06:02:31 +1100

    Reusable text classes

--- components/HitPointResultsTable.tsx
+++ components/HitPointResultsTable.tsx
@@ -28,12 +28,10 @@ const HitPointResultsTable: React.FC<HitPointResultsTableProps> = ({
     <div className="hit-point-results-table">
       {hitPointResults && hitDice && (
         <header>
-          <h2 className="flex items-baseline justify-center gap-1 pb-[1ch]">
-            <span className="flex-none text-3xl md:text-4xl lg:text-5xl">
-              {hitDice}
-            </span>
+          <h2 className="flex flex-wrap items-baseline justify-center gap-1 pb-[1ch]">
+            <span className="text-3xl md:text-4xl lg:text-5xl">{hitDice}</span>
             {monsterName && (
-              <small className="flex-shrink truncate text-lg md:text-xl">{`(${monsterName})`}</small>
+              <small className="text-lg md:text-xl">{`(${monsterName})`}</small>
             )}
           </h2>
         </header>
@@ -49,7 +47,7 @@ const HitPointResultsTable: React.FC<HitPointResultsTableProps> = ({
                   'min-w-[3ch] sm:min-w-[4ch] min-h-[1rem] py-[0.5ch] align-baseline text-center border-b',
                   hitPointResults
                     ? 'font-medium border-neutral-400'
-                    : 'border-neutral-200 text-neutral-200 font-light',
+                    : 'border-neutral-200 text-lightest font-light',
                 )}
                 style={
                   hitPointResults && {
@@ -65,11 +63,7 @@ const HitPointResultsTable: React.FC<HitPointResultsTableProps> = ({
           </tr>
         </tbody>
         <tfoot className="text-xs sm:text-sm md:text-base">
-          <tr
-            className={
-              hitPointResults ? 'text-neutral-500' : 'text-neutral-300'
-            }
-          >
+          <tr className={hitPointResults ? 'text-light' : 'text-lighter'}>
             {Object.keys(valueTexts).map((key) => (
               <th key={key} className="pt-2 font-normal">
                 {valueTexts[key as HitPointResultsKey]}
